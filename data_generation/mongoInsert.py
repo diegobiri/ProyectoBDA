a089caa0238eca5b503c3d6da599376b29016624
Vespertino <Vespertino@C14PC03.informatica.comercio>
2024-05-03 17:14:58 +0200

    4 CambioDockerCompose Carpetas

---
+++ data_generation/mongoInsert.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+import json
+
+def read_json_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        return None
+    
+class MongoDBOperations:
+    def __init__(self, database_name, port,username, password):
+        if username and password:
+            self.client = MongoClient(f'mongodb://{username}:{password}@localhost:{{port}}/')
+        else:
+            self.client = MongoClient(f'mongodb://localhost:{port}/')
+        self.db = self.client[database_name]
+        
+    def create_clientes(self, collection_name,data):
+        self.collection = self.db[collection_name]
+        result = self.collection.insert_one(data)
+        return result
+    
+clientes=read_json_file("ProyectoBDA/data_Prim_ord/json/clientes.json")
+
+
+mongo_operations = MongoDBOperations('Clientes','27017','bda','bdaPass')
+
+for data in clientes:
+    mongo_operations.create_clientes("clientes",data)
