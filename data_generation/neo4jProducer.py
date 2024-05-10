2128f0333de00138bdc85e36c9a158ed1e32af16
Vespertino <Vespertino@C14PC03.informatica.comercio>
2024-05-10 20:17:35 +0200

    11 SubidaDockerComposeNeo4j

--- data_generation/neo4jProducer.py
+++ data_generation/neo4jProducer.py
@@ -1,12 +1,10 @@
 from neo4j import GraphDatabase
 import json
 
+# Detalles de conexión a Neo4j
+neo4j_driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))  # Sustituir 'password' por tu contraseña de Neo4j
 
-# Neo4j connection details
-neo4j_driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))  # Replace 'your_password' with your Neo4j password
-
-
-# Obtenemos los datos de NEO4J
+# Funciones para obtener datos de NEO4J
 def fetch_neo4j_menus():
  with neo4j_driver.session() as session:
         result = session.run("MATCH (n:Menus) RETURN n")
@@ -19,21 +17,32 @@ def fetch_neo4j_platos():
 
 
 def fetch_neo4j_relaciones():
- with neo4j_driver.session() as session:
-        result = session.run("MATCH p=()-[r:ESTA]->() RETURN p")
-        return list(result)
+    with neo4j_driver.session() as session:
+        result = session.run("MATCH ()-[r:ESTA]->() RETURN r")
+        return [record["r"] for record in result]
 
 
+combined_data = []
 menus = fetch_neo4j_menus()
 platos = fetch_neo4j_platos()
 relaciones = fetch_neo4j_relaciones()
 
-if isinstance(relaciones, list):
-    combined_data = relaciones + menus + platos
-else:
-    combined_data = [relaciones] + menus + platos
+# Combinar datos según las relaciones
+for menu in menus:
+    for plato in platos:
+        combined_data.append({
+            "menu_item_id": menu[0]['id_menu'],
+            "precio": menu[0]['precio'],
+            "disponibilidad": menu[0]['disponibilidad'],
+            "restaurante_item_id": menu[0]['id_restaurante'],
+            "plato_item_id": plato[0]['id_plato'],
+            "nombre": plato[0]['nombre'],
+            "ingredientes": plato[0]['ingredientes'],
+            "alergenos": plato[0]['alergenos']
+        })
+        break
 
 # Escribir el archivo JSON resultante
-with open('ProyectoBDA/data_Prim_ord/json/conbined.json', 'w', encoding='utf-8') as file:
+with open('combined_data.json', 'w', encoding='utf-8') as file:
     json.dump(combined_data, file, ensure_ascii=False, indent=4)
 
